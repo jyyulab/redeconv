6123583922e3f55c7a1e05870e83d189e7237272
Honglei Zhou <honglei.zhou@stjude.org>
2025-04-07 09:24:26 -0500

    change version name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="redeconv",
-    version="1.1.1",
+    version="1.2.1",
     packages=find_packages(),
     install_requires=[
         "matplotlib==3.7.2",
